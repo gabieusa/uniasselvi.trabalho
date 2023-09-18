ca8a8a60f1ea432942bd9bdcec9443a2f985a698
Gabriela <84246788+gabieusa@users.noreply.github.com>
2023-09-18 09:13:07 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+<!-- ... Meu código HTML existente ... -->
+
+<footer>
+    <img src="logo-branco.png">
+    <p class="copyright">&copy; Copyright Barbearia GGR - 2023</p>
+</footer>
+
+<!-- Adicione o JavaScript aqui -->
+<script src="script.js"></script>
+
+</body>
+</html>
