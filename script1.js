44df9b36ecba002397c77544fd573df9c09fc6b9
Gabriela <84246788+gabieusa@users.noreply.github.com>
2023-09-18 09:16:25 -0300

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Contato - Barbearia GGR</title>
+
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+    <Link href="https://gabieusa.github.io/uniasselvi.trabalho/style.css" rel="stylesheet"/>
+</head>
+<body>
+    <header>
+        <div class="caixa">
+            <h1><img src="logo.png" alt="Logo da Barbearia GGR"></h1>
+
+            <nav>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="produtos.html">Produtos</a></li>
+                    <li><a href="contato.html">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <!-- Seu formulário e conteúdo aqui -->
+    </main>
+    <footer>
+        <img src="logo-branco.png" alt="Logo da Barbearia Alura">
+        <p class="copyright">&copy; Copyright Barbearia GGR -2023</p>
+    </footer>
+
+    <!-- Adicione o JavaScript aqui -->
+    <script src="script1.js"></script>
+</body>
+</html>
